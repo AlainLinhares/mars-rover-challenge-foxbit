f5967a8825af5da2bbb2e5246d83bc50bc3ea735
Alain Melo <arllmcomputacao@gmail.com>
2025-08-18 15:54:04 -0300

    chore: Initial project setup with Docker, Jest, and TypeScript configuration files

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(.*)\\.(css|less|scss)$': 'identity-obj-proxy'
+  }
+};
+module.exports = config;
